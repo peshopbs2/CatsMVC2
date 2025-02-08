d5909fc82f0202742541db4c0de1b8d304b6ba56
peshopbs2 <peshopbs2@gmail.com>
2025-02-08 00:22:17 -0800

    add cat repository

---
+++ CatsMVC/Repositories/Abstractions/ICatsRepository.cs
@@ -0,0 +1,14 @@
+﻿using CatsMVC.Data.Entities;
+
+namespace CatsMVC.Repositories.Abstractions
+{
+    public interface ICatsRepository
+    {
+        Task CreateAsync(Cat cat);
+        Task<Cat> UpdateAsync(Cat cat);
+        Task DeleteByIdAsync(int id);
+        Task<ICollection<Cat>> GetAllAsync();
+        ICollection<Cat> GetByFilter(Func<Cat, bool> predicate);
+        Task<Cat?> GetByIdAsync(int id);
+    }
+}
---
+++ CatsMVC/Repositories/CatsRepository.cs
@@ -0,0 +1,59 @@
+﻿using CatsMVC.Data;
+using CatsMVC.Data.Entities;
+using CatsMVC.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatsMVC.Repositories
+{
+    public class CatsRepository : ICatsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CatsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Cat cat)
+        {
+            _context.Add(cat);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            Cat cat = await GetByIdAsync(id);
+            if (cat != null)
+            {
+                _context.Remove(cat);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new ArgumentException($"Item with id {id} does not exist.");
+            }
+        }
+
+        public async Task<ICollection<Cat>> GetAllAsync()
+        {
+            return await _context.Cat.ToListAsync();
+        }
+
+        public ICollection<Cat> GetByFilter(Func<Cat, bool> predicate)
+        {
+            return _context.Cat
+                .Where(predicate)
+                .ToList();
+        }
+
+        public async Task<Cat?> GetByIdAsync(int id)
+        {
+            return await _context.Cat.FindAsync(id);
+        }
+
+        public async Task<Cat> UpdateAsync(Cat cat)
+        {
+            _context.Cat.Update(cat);
+            await _context.SaveChangesAsync();
+            return cat;
+        }
+    }
+}
