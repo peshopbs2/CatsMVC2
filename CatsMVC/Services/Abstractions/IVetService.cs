52ff47826ca1ae13a120e83ba2658a5a817d8d2d
peshopbs2 <peshopbs2@gmail.com>
2025-02-15 01:37:46 -0800

    add vet service

---
+++ CatsMVC/Services/Abstractions/IVetService.cs
@@ -0,0 +1,14 @@
+﻿using CatsMVC.DTOs;
+
+namespace CatsMVC.Services.Abstractions
+{
+    public interface IVetService
+    {
+        Task<VetDTO> GetByIdAsync(int id);
+        Task<ICollection<VetDTO>> GetAllAsync();
+        Task CreateAsync(VetDTO vetDto);
+        Task UpdateAsync(VetDTO vetDto);
+        Task DeleteAsync(int vetId);
+        ICollection<VetDTO> GetByName(string name);
+    }
+}
---
+++ CatsMVC/Services/VetService.cs
@@ -0,0 +1,56 @@
+﻿using CatsMVC.DTOs;
+using CatsMVC.Data.Entities;
+using CatsMVC.Repositories.Abstractions;
+using CatsMVC.Services.Abstractions;
+using AutoMapper;
+
+namespace CatsMVC.Services
+{
+    public class VetService : IVetService
+    {
+        private IVetRepository _vetRepository;
+        private IMapper _mapper;
+        public VetService(IVetRepository vetRepository, IMapper mapper)
+        {
+            _vetRepository = vetRepository;
+            _mapper = mapper;
+        }
+
+        public async Task CreateAsync(VetDTO vetDto)
+        {
+            Vet vet = _mapper.Map<Vet>(vetDto);
+            await _vetRepository.CreateAsync(vet);
+        }
+
+        public async Task DeleteAsync(int vetId)
+        {
+            await _vetRepository.DeleteByIdAsync(vetId);
+        }
+
+        public async Task<ICollection<VetDTO>> GetAllAsync()
+        {
+            var vets = await _vetRepository.GetAllAsync();
+            return _mapper.Map<ICollection<VetDTO>>(vets);
+        }
+
+        public async Task<VetDTO> GetByIdAsync(int id)
+        {
+            var vet = await _vetRepository.GetByIdAsync(id);
+            return _mapper.Map<VetDTO>(vet);
+        }
+
+        public ICollection<VetDTO> GetByName(string name)
+        {
+            var vets = _vetRepository
+                .GetByFilter(vet => vet.FirstName == name 
+                    || vet.LastName == name);
+            return _mapper.Map<ICollection<VetDTO>>(vets);
+        }
+
+        public async Task UpdateAsync(VetDTO vetDto)
+        {
+            Vet vet = _mapper.Map<Vet>(vetDto);
+            await _vetRepository.UpdateAsync(vet);
+        }
+    }
+}
