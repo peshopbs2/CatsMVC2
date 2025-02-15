68703f6fe407e10045e2976ef611164d58997311
peshopbs2 <peshopbs2@gmail.com>
2025-02-15 01:28:31 -0800

    add vet data transfer object

---
+++ CatsMVC/DTOs/VetDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CatsMVC.DTOs
+{
+    public class VetDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ CatsMVC/Profiles/VetProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CatsMVC.Data.Entities;
+using CatsMVC.DTOs;
+
+namespace CatsMVC.Profiles
+{
+    public class VetProfile : Profile
+    {
+        public VetProfile()
+        {
+            CreateMap<Vet, VetDTO>()
+                .ReverseMap();
+        }
+    }
+}
